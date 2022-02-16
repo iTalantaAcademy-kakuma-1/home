c2f0c2618b9d277aec0fc0fd2c9f81c04c14e6e5
Kambale Kibeho <dessaikibeho@gmail.com>
2022-02-16 15:03:40 +0300

    My-Vocabulary

---
+++ snippets/welcome-to-js/Dessai Kibeho/javascript.js
@@ -0,0 +1,19 @@
+//KeyWords and Variable  
+var Name='Dessai'
+alert("Alert N*1: A key word here is 'Var' and a variables is 'Name' and the declaration is:" + Name);
+//Operators
+let Num1= Number( prompt("Put A Number1"));
+
+let Num2= Number(prompt("Put a Number2"))
+let result= Num1 + Num2
+
+alert("Alert N*2: For Operators: prompt1+prompt2 = " +result);
+//Expressions
+let i=5;
+let i1=3;
+let Result1=i+i1
+alert("Alert N*3: the '=' signe, is an expression wich helps to attribute a Value. Here '=' helps to attribute to 'i' a value wich is "+i+". also '+' here is a expression helps to do an operation. For Example: 'i=5'+'i1=3' = "+Result1)
+//Statement
+var sum=5+7;
+alert( "All this is stament:'Var sum=5+7;' and the Result of that statement  is in console, open the console")
+console.log(sum);
