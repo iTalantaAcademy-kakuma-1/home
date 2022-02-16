eb1351eb0d4e72fac3c8fb900a636cdb02dbb429
noellamwanzia <39863136+noellamwanzia@users.noreply.github.com>
2022-02-16 13:10:30 +0300

    :package: snippets by paul

---
+++ snippets/welcome-to-js/paul.snippets/variables.js
@@ -0,0 +1,11 @@
+// Create an Array:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Change an element:
+cars[0] = "Toyota";
+
+// Add an element:
+cars.push("Audi");
+
+// Display the Array:
+document.getElementById("demo").innerHTML = cars; 
