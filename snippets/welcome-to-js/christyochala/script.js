91731d8b8d25e370364105000a0307edfd791b48
angathanga7878ochalla <93644090+angathanga7878ochalla@users.noreply.github.com>
2022-02-17 09:37:24 +0300

    committing my snippet js

---
+++ snippets/welcome-to-js/christyochala/script.js
@@ -0,0 +1,16 @@
+let name = 'Papi'
+// split each character
+alert(name.split())
+alert(name.split(''))
+
+let myNameArray = name.split('')
+// access P
+alert(name.charAt(0));
+alert(myNameArray[0]);
+
+
+
+
+
+
+
