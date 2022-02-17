f99f2ccc54285a5b70cd07ff0d9f152c0ac33284
angathanga7878ochalla <93644090+angathanga7878ochalla@users.noreply.github.com>
2022-02-17 10:01:27 +0300

    Update script.js

--- snippets/welcome-to-js/christyochala/script.js
+++ snippets/welcome-to-js/christyochala/script.js
@@ -8,6 +8,23 @@ let myNameArray = name.split('')
 alert(name.charAt(0));
 alert(myNameArray[0]);
 
+let name1 = ['Almarat', 'Arnu', 'lodi'];
+// Almarat
+alert(name1[0]);
+// Arnu
+alert(name1[1]);
+// Lodi
+alert(name1[2]);
+// full name
+alert(name1[0]+ " " +name1[1]+ " " +name1[2]);
+
+// using prompt method
+var name2 = prompt("enter name")
+name2.toString();
+document.write(name2);
+
+
+
 
 
 
