7b3cd2cfda0ae8de992d456ce8f182ec7c0ce437
abriyusif <abriyusif20@gmail.com>
2022-02-17 08:55:03 +0300

    added some methods

--- snippets/welcome-to-js/Abri/script.js
+++ snippets/welcome-to-js/Abri/script.js
@@ -1,3 +1,22 @@
 let text = "The rain in SPAIN stays mainly in the plain";
 text.match("ain");
-alert(text.match(/ain/))
+alert(text.match(/ain/));
+
+//using search
+
+const paragraph = 'The quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?';
+
+// any character that is not a word character or whitespace
+const regex = /[^\w\s]/g;
+
+alert(paragraph.search(regex));
+// expected output: 43
+
+alert(paragraph[paragraph.search(regex)]);
+// expected output: "."
+
+//Using toString method
+
+var x = new String('Hello world');
+
+alert(x.toString());
