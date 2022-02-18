b43e244f24d0ae624ab69f37fdf665c6b1061a39
achiekayuen <93641682+achiekayuen@users.noreply.github.com>
2022-02-18 09:33:43 +0300

    new updates

---
+++ snippets/script.js
@@ -0,0 +1,12 @@
+// join syntax
+
+let name = ['Achiek' , 'Ayuen']
+console.log(name.join(' '));
+
+// Trim
+let myText = "Hello Guys";
+console.log(myText.trim());
+
+var subject1 = 'Bio'
+var subject2 = 'Chem'
+console.log(subject1.concat(subject2));
